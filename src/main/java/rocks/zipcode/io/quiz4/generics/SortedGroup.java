55d4a5039560308fd3b6b2d9e32c1c68fefc20a9
Angela Kim <aangelakkim@gmail.com>
2023-09-11 15:19:14 -0400

    started quiz

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,35 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author leon on 18/12/2018.
  */
-public class SortedGroup<_> extends Group<_> {
+public class SortedGroup<T extends Comparable<T>> extends Group<T> {
+
+    List<T> list;
+
+    public SortedGroup() {
+        list = new ArrayList<>();
+    }
     @Override
-    public void insert(_ value) {
+    public void insert(T value) {
+        list.add(value);
+        Collections.sort(list);
     }
 
+
     @Override
-    public void delete(_ value) {
+    public void delete(T value) {
+        list.remove(value);
     }
 
-    public Integer indexOf(_ value) {
-        return null;
+    public Integer indexOf(T value) {
+        return list.indexOf(value);
     }
+
+
 }
